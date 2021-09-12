7e60f0fbb4c7381125f93fed193f32cb17b00874
zikri-muhammad <muhammadzikri598@gmail.com>
2021-09-12 23:47:43 +0700

    hash table

---
+++ data-structur-array/hashCollesion.js
@@ -0,0 +1,13 @@
+let user = {
+    name: 'Muhammad Zikri',
+    age : 25,
+    magic : true,
+    scream : () => {
+        console.log('Ahaaaa!!')
+    }
+    
+}
+
+console.log(user.age)
+console.log(user.spell = 'abra ktabra')
+console.log(user.scream())
---
+++ data-structur-array/hashTable.js
@@ -0,0 +1,44 @@
+class HashTable {
+    constructor(size){
+        this.data = new Array(size);
+    }
+
+    _hash(key) {
+        let hash = 0;
+        for(let i =0; i < key.length; i++){
+            hash = (hash + key.charCodeAt(i) * i) %
+            this.data.length
+        }
+        return hash;
+    }
+
+    set(key, value) {
+        let address = this._hash(key);
+        if(!this.data[address]) {
+            this.data[address] = [];
+        }
+        this.data[address].push([key, value])
+        return this.data
+
+    }
+
+    get(key){
+        let address = this._hash(key);
+        const currentBucket = this.data[address];
+        if(currentBucket){
+            for (let i = 0; i < currentBucket.length; i++) {
+                if(currentBucket[i][0] === key){
+                    return currentBucket[i][1]
+                }
+            }
+        }
+        return undefined
+    }
+}
+
+
+
+const myHashTable = new HashTable(50);
+console.log(myHashTable.set('grapes', 100000));
+console.log(myHashTable.set('aplle', 56));
+console.log(myHashTable.get('grapes'));
