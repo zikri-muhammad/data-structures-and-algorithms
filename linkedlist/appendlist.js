b46eb045ba7a1b2fcef3435412754eeb01a4218b
zikri-muhammad <muhammadzikri598@gmail.com>
2021-09-19 07:42:03 +0700

    linked list insert

--- linkedlist/appendlist.js
+++ linkedlist/appendlist.js
@@ -32,8 +32,8 @@ class LinkedList {
     }
 
     printList(){
-        const array = [];
-        let currentNode = this.head;
+        const array       = [];
+        let currentNode   = this.head;
         while(currentNode !== null){
             array.push(currentNode.value);
             currentNode = currentNode.next;
@@ -42,7 +42,34 @@ class LinkedList {
     }
     
     insert(index, value){
+        // check params
+        if(index >= this.length){
+            return this.append(value);
+        }
 
+        const newNode = {
+            value   : value,
+            next    : null
+        }
+
+        const leader = this.traverseToIndex(index - 1);
+        const holdingPointer = leader.next;
+        leader.next = newNode
+        newNode.next = holdingPointer;
+        this.length++;
+        return this.printList()
+        
+    }
+
+    traverseToIndex(index){
+        //check params
+        let counter = 0;
+        let currentNode = this.head;
+        while(counter !== index){
+            currentNode = currentNode.next;
+            counter++;
+        }
+        return currentNode
     }
 
 }
@@ -52,6 +79,6 @@ const myLinkedList = new LinkedList(10)
 myLinkedList.append(5)
 myLinkedList.append(16)
 myLinkedList.prepend(1)
+myLinkedList.insert(2, 99)
 console.log(myLinkedList.printList())
-// myLinkedList.insert(2, 99)
 // console.log(myLinkedList) 
