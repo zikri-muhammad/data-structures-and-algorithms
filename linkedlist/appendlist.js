6d98ac0b68a3567b7e636f6ae2008544e499295c
zikri-muhammad <muhammadzikri598@gmail.com>
2021-09-19 07:30:53 +0700

    linked list printlist method

--- linkedlist/appendlist.js
+++ linkedlist/appendlist.js
@@ -30,6 +30,21 @@ class LinkedList {
         this.length++;
         return this
     }
+
+    printList(){
+        const array = [];
+        let currentNode = this.head;
+        while(currentNode !== null){
+            array.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+        return array
+    }
+    
+    insert(index, value){
+
+    }
+
 }
 
 
@@ -37,4 +52,6 @@ const myLinkedList = new LinkedList(10)
 myLinkedList.append(5)
 myLinkedList.append(16)
 myLinkedList.prepend(1)
-console.log(myLinkedList) 
+console.log(myLinkedList.printList())
+// myLinkedList.insert(2, 99)
+// console.log(myLinkedList) 
