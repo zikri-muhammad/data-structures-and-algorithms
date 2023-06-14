955c145802fdf85a0cd5e504afe52b2fb61dc532
zikri-muhammad <muhammadzikri598@gmail.com>
2023-06-14 09:56:35 +0700

    tryning class and solved reverse string using array

--- data-structur-array/class.js
+++ data-structur-array/class.js
@@ -1,47 +1,74 @@
-var object1 = {value : 10};
-var object2 = object1
-var object3 = {value : 10};
+// var object1 = {value : 10};
+// var object2 = object1
+// var object3 = {value : 10};
 
-/* 
-* context vs scope
-*/
+// /* 
+// * context vs scope
+// */
 
-const object4 = {
-    a: function() {
-        console.log(this)
-    }
-}
+// const object4 = {
+//     a: function() {
+//         console.log(this)
+//     }
+// }
 
 
-/* 
-* instantiation
-*/
+// /* 
+// * instantiation
+// */
 
-class Player {
+// class Player {
+//     constructor(name, type){
+//         console.log(this);
+//         this.name = name
+//         this.type = type
+//     }
+//     introduce(){
+//         console.log(`Hi I am ${this.name}, I'm a ${this.type}`)
+//     }
+// }
+
+// class Wizzard extends Player {
+//     constructor(name, type){
+//         super(name, type)
+//     }
+//     play(){
+//         console.log(`Weeeeeeee I'm ${this.type}`)
+//     }
+// }
+
+
+// const wizzard1 = new Wizzard('Shuna', 'Healer');
+// const wizzard2 = new Wizzard('diablo', 'Dark Magic');
+
+// console.log(wizzard1);
+// console.log(wizzard2);
+// console.log(wizzard1.play());
+// console.log(wizzard2.introduce());
+
+
+class Parner {
     constructor(name, type){
-        console.log(this);
         this.name = name
         this.type = type
     }
     introduce(){
-        console.log(`Hi I am ${this.name}, I'm a ${this.type}`)
+        console.log(`Hi My name ${this.name} type parner ${this.type}`); 
     }
 }
 
-class Wizzard extends Player {
+class Colleague extends Parner {
     constructor(name, type){
         super(name, type)
     }
-    play(){
-        console.log(`Weeeeeeee I'm ${this.type}`)
+    work(){
+        console.log(`We Are ${this.type}`);
     }
 }
 
-
-const wizzard1 = new Wizzard('Shuna', 'Healer');
-const wizzard2 = new Wizzard('diablo', 'Dark Magic');
-
-console.log(wizzard1);
-console.log(wizzard2);
-console.log(wizzard1.play());
-console.log(wizzard2.introduce());
+const play = new Colleague('muhammad zikri', 'oyi')
+const play1 = new Colleague('muhammad zikri', 'oyi')
+console.log(play);
+console.log(play1);
+console.log(play.work());
+console.log(play1.introduce());
--- data-structur-array/reverse.js
+++ data-structur-array/reverse.js
@@ -1,29 +1,59 @@
-function reverse(str){
-    // check input 
-    if(!str || typeof str != 'string' || str.length < 2) 
-    return str;
+// function reverse(str){
+//     // check input 
+//     if(!str || typeof str != 'string' || str.length < 2) 
+//     return str;
 
-    const backwards = [];
-    const totalItems = str.length - 1;
-    for (let i = totalItems; i >= 0; i--) {
-        backwards.push(str[i])
+//     const backwards = [];
+//     const totalItems = str.length - 1;
+//     for (let i = totalItems; i >= 0; i--) {
+//         backwards.push(str[i])
         
+//     }
+
+//     return backwards.join(''); 
+// }
+
+
+// function reverse2(str){
+//     // check for valid input
+//     return str.split('').reverse().join('');
+// }
+
+// const reverse3 = str => [...str].reverse().join('');
+
+// const reverses = reverse('Muhammad Zikri')
+// const reverses2 = reverse2('Muhammad Zikri')
+// const reverses3 = reverse2('Muhammad Zikri')
+// console.log(reverses)
+// console.log(reverses2)
+// console.log(reverses3)
+
+
+// const reverse = (str) => {
+//     if (!str || typeof str != 'string' || str < 0) return str
+    
+//     // const backwards = []
+//     let reversedStr = ''
+//     const lastIndex = str.length - 1
+//     console.log(lastIndex);
+//     for (let index = lastIndex; index >= 0; index--) {
+//         reversedStr += str[index]
+//     }
+    
+//     return reversedStr
+// }
+
+const reverse = (str) => {
+    if (!str || typeof str != 'string' || str < 0) return str
+    
+    const backwards = []
+    const lastIndex = str.length - 1
+    console.log(lastIndex);
+    for (let index = lastIndex; index >= 0; index--) {
+        backwards.push(str[index])
     }
-
-    return backwards.join(''); 
+    
+    return backwards.join('')
 }
 
-
-function reverse2(str){
-    // check for valid input
-    return str.split('').reverse().join('');
-}
-
-const reverse3 = str => [...str].reverse().join('');
-
-const reverses = reverse('Muhammad Zikri')
-const reverses2 = reverse2('Muhammad Zikri')
-const reverses3 = reverse2('Muhammad Zikri')
-console.log(reverses)
-console.log(reverses2)
-console.log(reverses3)
+console.log(reverse('muhammad zikri'));
